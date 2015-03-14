214cf7338c14a43c29ad223421cc8b4838185025
Simon Taranto <simon.taranto@gmail.com>
2015-03-14 12:17:54 -0700

    Remove comments from route

--- frontend/app/routes/sites.js
+++ frontend/app/routes/sites.js
@@ -1,19 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // get sites from server
   model: function() {
     return this.store.find('site');
-  },
-
-  x_model: function() {
-    return [
-      { id: 1, name: "site 1", images: [
-        { id: 1, captured_at: "1/1/2015", url: "http://foo.com", site_id: 1 },
-        { id: 2, captured_at: "2/2/2015", url: "http://bar.com", site_id: 1 }
-      ] },
-      { id: 2, name: "site 2", images: [] },
-      { id: 3, name: "site 3", images: [] },
-    ];
   }
 });
