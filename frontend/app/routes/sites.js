d06939fdd3e4d62410996691670c60105465a979
Simon Taranto <simon.taranto@gmail.com>
2015-03-14 11:16:41 -0700

    Add site model and create call to api/sites

--- frontend/app/routes/sites.js
+++ frontend/app/routes/sites.js
@@ -3,6 +3,10 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   // get sites from server
   model: function() {
+    return this.store.find('site');
+  },
+
+  x_model: function() {
     return [
       { id: 1, name: "site 1", images: [
         { id: 1, date: "1/1/2015", url: "http://foo.com" },
