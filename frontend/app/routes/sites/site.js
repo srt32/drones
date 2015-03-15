b3c2ede0142541302664675044cbf98f43d58c40
Simon Taranto <simon.taranto@gmail.com>
2015-03-15 16:36:56 -0700

    Transition to first image on site show page

--- frontend/app/routes/sites/site.js
+++ frontend/app/routes/sites/site.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  afterModel: function(site, transition) {
+    this.transitionTo('sites.site.images.image', site.get('images').get('firstObject'));
+  }
 });
