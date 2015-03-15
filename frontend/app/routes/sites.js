b3c4a56a5ce8637b61405520a351da142006b1d8
Simon Taranto <simon.taranto@gmail.com>
2015-03-15 16:35:09 -0700

    Transition to first site show from root

--- frontend/app/routes/sites.js
+++ frontend/app/routes/sites.js
@@ -3,5 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('site');
+  },
+
+  afterModel: function(sites, transition) {
+    this.transitionTo('sites.site', sites.get('firstObject'));
   }
 });
