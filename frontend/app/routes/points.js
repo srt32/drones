bf897341b671a3e989fb0f48a05fd794d462aa76
Simon Taranto <simon.taranto@gmail.com>
2015-03-15 18:46:39 -0700

    WIP

--- frontend/app/routes/points.js
+++ frontend/app/routes/points.js
@@ -4,6 +4,12 @@ export default Ember.Route.extend({
   model: function() {
     var image = this.modelFor("image");
     var points = this.store.find("point", { image_id: image.get("id") });
+
+    console.log("length of points is", points.length);
+
+    points.forEach(function(point) {
+      image.points.pushObject(point);
+    });
     // need to append to the image model??
 
     return points;
